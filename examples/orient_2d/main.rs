c9076f30afb6badfe77102f994ce9e44b16fd5ef
glennDittmann <glenn.dittmann@posteo.de>
2024-04-29 18:02:08 +0200

    Refactor examples duplicate code

--- examples/orient_2d/main.rs
+++ examples/orient_2d/main.rs
@@ -1,7 +1,8 @@
 
 use geogram_predicates::orient_2d;
-use std::cmp::Ordering;
 use std::path::Path;
+use test_utils::{predicate_2d_test, write_to_png};
+
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
@@ -22,7 +23,7 @@ fn main() {
         _ => unimplemented!(),
     };
     
-    let predicate_results = orient_2d_test(predicate, [0.5, 0.5], 256, 256);
+    let predicate_results = predicate_2d_test(predicate, [0.5, 0.5], 256, 256);
     
     let out_path = format!("out_{}_orient_2d.png", mode);
     
@@ -32,52 +33,6 @@ fn main() {
 // Refer: https://www.cs.cmu.edu/~quake/robust.html
 fn naive_orient_2d(a: &[f64; 2], b: &[f64; 2], c: &[f64; 2]) -> f64 {
     (b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0])
-}
-
-fn orient_2d_test<F>(predicate: F, start: [f64; 2], width: usize, height: usize) -> Vec<Ordering>
-where
-    F: Fn([f64; 2]) -> f64,
-{
-    use float_extras::f64::nextafter;
-    let mut yd = start[1];
-    let mut data = Vec::with_capacity(width * height);
-
-    for _ in 0..height {
-        let mut xd = start[0];
-        for _ in 0..width {
-            let p = [xd, yd];
-            data.push(predicate(p).partial_cmp(&0.).unwrap());
-            xd = nextafter(xd, std::f64::INFINITY);
-        }
-        yd = nextafter(yd, std::f64::INFINITY);
-    }
-
-    data
-}
-
-fn write_to_png(data: &[Ordering], path: &Path, width: usize, height: usize) {
-    assert_eq!(data.len(), width * height);
-
-    use std::fs::File;
-    use std::io::BufWriter;
-
-    let file = File::create(path).unwrap();
-    let ref mut w = BufWriter::new(file);
-
-    let mut encoder = png::Encoder::new(w, width as u32, height as u32);
-    encoder.set_color(png::ColorType::Grayscale);
-    encoder.set_depth(png::BitDepth::Eight);
-
-    let mut writer = encoder.write_header().unwrap();
-    let data = data
-        .iter()
-        .map(|w| match w {
-            Ordering::Less => 0u8,
-            Ordering::Equal => 127,
-            Ordering::Greater => 255,
-        })
-        .collect::<Vec<_>>();
-    writer.write_image_data(&data).unwrap();
 }
 
 fn usage(name: &str) -> ! {
---
+++ test_utils/src/lib.rs
@@ -0,0 +1,46 @@
+use float_extras::f64::nextafter;
+use std::{cmp::Ordering, path::Path};
+
+pub fn write_to_png(data: &[Ordering], path: &Path, width: usize, height: usize) {
+    assert_eq!(data.len(), width * height);
+
+    use std::fs::File;
+    use std::io::BufWriter;
+
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, width as u32, height as u32);
+    encoder.set_color(png::ColorType::Grayscale);
+    encoder.set_depth(png::BitDepth::Eight);
+
+    let mut writer = encoder.write_header().unwrap();
+    let data = data
+        .iter()
+        .map(|w| match w {
+            Ordering::Less => 0u8,
+            Ordering::Equal => 127,
+            Ordering::Greater => 255,
+        })
+        .collect::<Vec<_>>();
+    writer.write_image_data(&data).unwrap();
+}
+
+pub fn predicate_2d_test<F>(predicate: F, start: [f64; 2], width: usize, height: usize) -> Vec<Ordering>
+where
+    F: Fn([f64; 2]) -> f64,
+{
+    let mut yd = start[1];
+    let mut data = Vec::with_capacity(width * height);
+
+    for _ in 0..height {
+        let mut xd = start[0];
+        for _ in 0..width {
+            let p = [xd, yd];
+            data.push(predicate(p).partial_cmp(&0.).unwrap());
+            xd = nextafter(xd, std::f64::INFINITY);
+        }
+        yd = nextafter(yd, std::f64::INFINITY);
+    }
+    data
+}
